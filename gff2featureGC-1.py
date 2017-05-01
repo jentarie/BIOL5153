cbaeccaca4dab12f8dc163e35d4279335c95a280
Jennifer Beals <jmbeals@uark.edu>
2017-05-01 13:47:08 -0500

    First sad attempt at getting fasta style output. Added complement function and estracted gene names

--- gff2featureGC-1.py
+++ gff2featureGC-1.py
@@ -10,21 +10,59 @@ def clean_seq(input_seq):
 def nuc_freq(sequence, base, sig_figs=2):
     #calculate the length of the sequence
     length = len(sequence)
-
-
+    
+    
     # count the number of this nucleotide
     base_count = sequence.count(base)
-
+    
     # calculate base frequency
     base_freq = base_count/length
-
+    
     # return the frequency and length of nucleotide
     return(length, round(base_freq, sig_figs))
+
+
+#reverse complement dna
+def rev_compl(fragment):
+    compl = fragment.replace("ACTG", "tgac")
+    return compl.upper()
+
+#function for G+C
+### Try to extract all the gene sequences from the gcc file for exons only.
+#fine if on the coding strand, but could be on the negative strang (function for reverse complement)
+#print/store/build a fasta sequence of each gene (coding sequence) eg:
+#>cox1
+#TGATTT...
+
+
+#key = feeature_type, value
+feature_sequences = {}
+
+#key = gene, value
+gene_sequences = {}
 
 gff_file = 'watermelon.gff'
 fsa_file = 'watermelon.fsa'
 gff_in = open(gff_file, 'r')
 fsa_in = open(fsa_file, 'r')
+
+
+#extract names of genes
+#def gene_extract(file):
+#    for line in file:
+#        if "CDS" in line:
+#            gene = line.split("Gene ")[1].split(";")[0]
+#            return gene
+#print(gene)
+#print(gene_extract(gff_in))
+
+gene_list = []
+for line in gff_in:
+    if "CDS" in line:
+        gene = line.split("Gene ")[1].split(" ;")[0]
+        if gene[0] not in gene_list:
+            gene_list.append(gene)
+print(gene_list)
 
 genome = ""
 
@@ -40,59 +78,51 @@ for line in fsa_in:
 
 fsa_in.close()
 
-cds = ""
-intron = ""
-tRNA = ""
-rRNA = ""
-repeats = ""
-misc = ""
-
 for line in gff_in:
 #    line = line.rstrip('\n')
 #    types = line.split('; type ')
 #    other_type = types[len(types)-1]
 #    print(other_type)
-
+    gene = line.split("Gene ")[1].split(" ;")[0]
     fields = line.split('\t')
     type = fields[2]
     start = int(fields[3])
     end = int(fields[4])
     #print(type, "\t", start, "\t", end)
     fragment = genome[start - 1:end]
+    fragment = clean_seq(fragment)
+    
+
+  
+    
+    if gene in gene_sequences:
+        gene_sequences[gene] += fragment
+    else:
+        gene_sequences[gene] = fragment
+
+  
+for gene, sequence in gene_sequences.items():
+    print(gene + '\t' + sequence)
 
 #    g_count = fragment.count('G')
 #    c_count = fragment.count('C')
-#    gc_count = g_count + c_count
+#    gc_count = g_count + c_count   
 
-    fragment = clean_seq(fragment)
+    #populate our dictionary
+#    if type in feature_sequences:
+#        feature_sequences[type] += fragment
+#    else:
+#        feature_sequences[type] = fragment
+    
+#gff_in.close()
+
+#for feature, sequence in feature_sequences.items():
+#    print(feature + '\t' + str(len(sequence)))
+#    #print(sequence)
+#    #call function here to calculate GC content
 
 
-    if type == 'CDS':
-        cds += fragment
-    if type == 'intron':
-        intron += fragment
-    if type == 'tRNA':
-        tRNA += fragment
-    if type == 'rRNA':
-        rRNA += fragment
-    if type == 'repeat_region':
-        repeats += fragment
-    if type == 'misc_feature':
-        misc += fragment
-#print(cds.count('G'))
-types = ['cds', 'intron', 'tRNA', 'rRNA', 'repeats', 'misc']
+
+types = ['cds', 'intron', 'tRNA', 'rRNA', 'repeats', 'misc']       
 #print(len(cds))
 #sys.exit()
-
-
-
-# loop over features:
-i = 0
-#for feature_type in [CDS, intron, tRNA, rRNA, repeat_region, misc_feature]:
-for feature_type in [cds, intron, tRNA, rRNA, repeats, misc]:
-    #loop over 4 nucleotides
-    for nucleotide in ['A', 'C', 'G', 'T']:
-        #calculate the nucleotide composition for each feature
-        (feature_length, feature_comp) = nuc_freq(sequence=feature_type, base=nucleotide, sig_figs=2)
-        print(types[i] + "\t\t" + str(feature_length) + ' nucleotides\t' + str(feature_comp) + "% " + nucleotide)
-    i = i + 1
