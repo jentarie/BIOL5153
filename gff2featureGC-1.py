c46e136caffc69ae3542e0bd71f8bb9085b74a06
Jennifer Beals <jmbeals@uark.edu>
2017-05-05 14:42:48 -0500

    almost correct, exons still separate, not concateneated into gene

--- gff2featureGC-1.py
+++ gff2featureGC-1.py
@@ -1,52 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 12:56:29 2017
+
+@author: jen
+"""
+
 #!/usr/bin/python
-
+ 
 import sys
-
+ 
 def clean_seq(input_seq):
     clean = input_seq.upper()
     clean = clean.replace('N', '')
     return clean
-
+ 
 def nuc_freq(sequence, base, sig_figs=2):
     #calculate the length of the sequence
     length = len(sequence)
-    
+   
     
     # count the number of this nucleotide
     base_count = sequence.count(base)
-    
+   
     # calculate base frequency
     base_freq = base_count/length
-    
+   
     # return the frequency and length of nucleotide
     return(length, round(base_freq, sig_figs))
-
-
+ 
+ 
 #reverse complement dna
 def rev_compl(fragment):
     compl = fragment.replace("ACTG", "tgac")
     return compl.upper()
-
+ 
 #function for G+C
 ### Try to extract all the gene sequences from the gcc file for exons only.
 #fine if on the coding strand, but could be on the negative strang (function for reverse complement)
 #print/store/build a fasta sequence of each gene (coding sequence) eg:
 #>cox1
 #TGATTT...
-
-
+#create dictionary genes; key= gene name, value=another dictionar(ies) {key=exon number, value = sequence}
+ 
+ 
+import collections
+#from collections import defaultdict
+ 
 #key = feeature_type, value
 feature_sequences = {}
-
-#key = gene, value
+ 
+#key = gene, value = another dictionary[key=exon number, value= sequence]
+# gene_sequences[cox1][1]=the sequence for the 1st exon of cox1
+# gene_sequences[cox1][2]=the sequence for the 2nd exon of cox1
+ 
+#tuple method - almost works!
 gene_sequences = {}
-
-gff_file = 'watermelon.gff'
-fsa_file = 'watermelon.fsa'
+ 
+#nested dictionaries method
+#Gene_sequences = defaultdict(dict)
+ 
+ 
+gff_file = '/home/jen/Desktop/BIOL5153/watermelon_files/watermelon.gff'
+fsa_file = '/home/jen/Desktop/BIOL5153/watermelon_files/watermelon.fsa'
 gff_in = open(gff_file, 'r')
 fsa_in = open(fsa_file, 'r')
-
-
+ 
+ 
 #extract names of genes
 #def gene_extract(file):
 #    for line in file:
@@ -55,19 +75,19 @@ fsa_in = open(fsa_file, 'r')
 #            return gene
 #print(gene)
 #print(gene_extract(gff_in))
-
-gene_list = []
-for line in gff_in:
-    if "CDS" in line:
-        gene = line.split("Gene ")[1].split(" ;")[0]
-        if gene[0] not in gene_list:
-            gene_list.append(gene)
-print(gene_list)
-
+ 
+#gene_list = []
+#for line in gff_in:
+#    if "CDS" in line:
+#        gene = line.split("Gene ")[1].split(" ;")[0]
+#        if gene[0] not in gene_list:
+#            gene_list.append(gene)
+#print(gene_list)
+ 
 genome = ""
-
+ 
 line_number = 0
-
+ 
 for line in fsa_in:
 #    print(str(line_number) + ": " + line )
     line = line.rstrip('\n')
@@ -75,54 +95,115 @@ for line in fsa_in:
         genome = genome + line
     line_number += 1
 #print(len(genome))
-
+ 
 fsa_in.close()
-
+ 
 for line in gff_in:
 #    line = line.rstrip('\n')
 #    types = line.split('; type ')
 #    other_type = types[len(types)-1]
 #    print(other_type)
-    gene = line.split("Gene ")[1].split(" ;")[0]
+    #gene = line.split("Gene ")[1].split(" ;")[0]
     fields = line.split('\t')
     type = fields[2]
     start = int(fields[3])
     end = int(fields[4])
     #print(type, "\t", start, "\t", end)
+   
+    #extract and clean sequence
     fragment = genome[start - 1:end]
-    fragment = clean_seq(fragment)
+    fragment = clean_seq(fragment)   
+ 
+    if (fields[6] == '-'):
+        fragment = rev_compl(fragment)
+ 
+#    if gene in gene_sequences:
+#        gene_sequences[gene] += fragment
+#    else:
+#        gene_sequences[gene] = fragment
+       
+    
+    
+    #get the gene name
+    if(type == 'CDS'):
+        attributes = fields[8].split(' ; ')
+        #print(attributes[0])
+       
+        gene_fields = attributes[0].split(' ')
+        gene_name = gene_fields[1]   
+    #determine if there are multiple nmber of exons
+    #get the exon number
+        if('exon' in gene_fields):
+            exon_num = gene_fields[-1]
+            #exon_num = gene_fields[3]
+            #print(gene_name, exon_num)
+        else:
+            exon_num = '0'
+ 
+        #gene_exons = dict(zip(zip(gene_name,exon_num),fragment))
+        #print(gene_sequenceS)
+        #print(fragment)
+       
+#        for gene_name, exon_num, fragment in gene_exons.items():
+#            if gene_name in gene_sequences:
+#                gene_sequnces[gene_name] += fragment
+#            else:
+#                gene_sequences[gene_name] = fragment
+ 
+#build dictionary using tuple as key
+        gene_sequences[(gene_name, exon_num)] = fragment
+#print(gene_sequences)   
+ 
+        
+for gene_name, exon_num in gene_sequences:
+    print(">" + gene_name + ' ' + str(exon_num) + '\n' + gene_sequences[(gene_name, exon_num)])
+   
+    
+#genes_compiled={}
+
+#genes_compiled = collections.OrderedDict(sorted(gene_sequences.items()))
+#print(genes_compiled)
+
+#for gene, exon_num, sequence in genes_compiled.items():
     
 
-  
-    
-    if gene in gene_sequences:
-        gene_sequences[gene] += fragment
-    else:
-        gene_sequences[gene] = fragment
+#for gene_name, exon_num in gene_sequences:
+#   if gene_name in genes_compiled:
+       
+        
 
-  
-for gene, sequence in gene_sequences.items():
-    print(gene + '\t' + sequence)
 
+
+
+#for gene_name, sequence in Gene_sequences.items():
+#    print(gene_name)
+#    print(sequence)
+       
+    #store the fragment in the gene_sequence dictionary
+ 
+ 
+#for gene, sequence in gene_sequences.items():
+#    print(gene + '\t' + sequence)
+ 
 #    g_count = fragment.count('G')
 #    c_count = fragment.count('C')
-#    gc_count = g_count + c_count   
-
+#    gc_count = g_count + c_count  
+ 
     #populate our dictionary
 #    if type in feature_sequences:
 #        feature_sequences[type] += fragment
 #    else:
 #        feature_sequences[type] = fragment
-    
+   
 #gff_in.close()
-
+ 
 #for feature, sequence in feature_sequences.items():
 #    print(feature + '\t' + str(len(sequence)))
 #    #print(sequence)
 #    #call function here to calculate GC content
-
-
-
-types = ['cds', 'intron', 'tRNA', 'rRNA', 'repeats', 'misc']       
+ 
+ 
+ 
+#types = ['cds', 'intron', 'tRNA', 'rRNA', 'repeats', 'misc']
 #print(len(cds))
 #sys.exit()
